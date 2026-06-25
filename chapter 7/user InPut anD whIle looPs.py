98fc6dc9433b4494f6babfec500288933048df92
Cheeseboycr7 <shezimandla20@gmail.com>
2026-06-25 13:25:02 +0200

    Moving Items from One List to Another AND Removing All Instances of Specific Values from a List AND Filling a Dictionary with User Input

--- chapter 7/user InPut anD whIle looPs.py
+++ chapter 7/user InPut anD whIle looPs.py
@@ -63,9 +63,9 @@ while True:
 
     print(f'The {message.upper()} is added to your pizza')
 
-
 '''
 
+'''
 while True:
     age = input("Enter your age (or type 'quit' to exit): ")
 
@@ -81,8 +81,111 @@ while True:
     else:
         print("Your ticket costs $15.")
 
+'''
+
+'''
+
+print('----------new code------------')
 
 
+7-8. Deli: Make a list called sandwich_orders and fill it with the names of vari
+ous sandwiches . Then make an empty list called finished_sandwiches . Loop 
+through the list of sandwich orders and print a message for each order, such 
+as I made your tuna sandwich. As each sandwich is made, move it to the list 
+of finished sandwiches . After all the sandwiches have been made, print a 
+message listing each sandwich that was made 
+
+
+sandwich_orders = ['Bread_Sandwich','hotdog','cheeseburger','pizza']
+
+finished_sandwiches = []
+
+while sandwich_orders:
+    current_sandwhich = sandwich_orders.pop()
+
+    print(f'\n I made you a {current_sandwhich.title()} ')
+    finished_sandwiches.append(current_sandwhich)
+
+
+
+    print('\nlist of each sandwich that was made ')
+    for finished_sandwiche  in finished_sandwiches :
+       
+
+     print(finished_sandwiche.title())
+'''
+
+
+'''
+7-9. No Pastrami: Using the list sandwich_orders from Exercise 7-8, make sure 
+the sandwich 'pastrami' appears in the list at least three times . Add code 
+near the beginning of your program to print a message saying the deli has 
+run out of pastrami, and then use a while loop to remove all occurrences of 
+'pastrami' from sandwich_orders . Make sure no pastrami sandwiches end up
+in finished_sandwiches 
+'''
+
+'''
+sandwich_orders = ['hotdog','cheeseburger','pizza','pastrami','pastrami','pastrami']
+
+
+print('\nthe deli has run out of pastrami')
+
+while 'pastrami' in sandwich_orders:
+    finished_pastrami = sandwich_orders.pop()
+    print(f'\n{finished_pastrami} is out of menu')
+
+
+print('\nUpdated Menu')
+for sand in sandwich_orders:
+    print(sand)
+'''
+
+'''
+sandwich_orders = ['hotdog','cheeseburger','pizza','pastrami','pastrami','pastrami']
+
+
+print('\nthe deli has run out of pastrami')
+
+while 'pastrami' in sandwich_orders:
+    finished_pastrami = sandwich_orders.pop()
+    print(f'\n{finished_pastrami} is out of menu')
+
+
+print('\nUpdated Menu')
+for sand in sandwich_orders:
+    print(sand)
+'''
+
+'''
+7-10. Dream Vacation: Write a program that polls users about their dream 
+vacation . Write a prompt similar to If you could visit one place in the world, 
+where would you go? Include a block of code that prints the results of the poll 
+'''
+
+
+D_Vacation = {}
+
+while True:
+    user = input('What is your name? ')
+    vacation = input('What is your dream vacation? ')
+
+    D_Vacation[user] = vacation
+
+    repeat = input("Would you like to let another person respond? (yes/ no) ")
+    if repeat == 'no':
+        break
+
+for user , vacation in D_Vacation.items():
+    print(f'{user} would like to visit {vacation}')
 
 
     
+
+
+
+
+
+
+
+
