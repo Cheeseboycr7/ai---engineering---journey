31aa0394adc33d2ab96c95f64914c03840b44912
Cheeseboycr7 <shezimandla20@gmail.com>
2026-06-28 20:04:35 +0200

    Creating Modules

---
+++ chapter 8/car.py
@@ -0,0 +1,10 @@
+from function import car_info
+
+'''
+8-16. Imports: Using a program you wrote that has one function in it, store that 
+function in a separate file . Import the function into your main program file, and 
+call the function using each of these approaches:
+import module_name
+'''
+
+car_info('Camry','Toyota',color= 'blue', available = True)
